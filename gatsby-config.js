2ab5365debcab92b863cdc774b725a62f8d04aec
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-08 02:06:33 -0600

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,7 @@ module.exports = {
     title: `Personal Site`,
     siteUrl: `https://ryand.ca`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
