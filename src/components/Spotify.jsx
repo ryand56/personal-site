18eab71d6fc1ef95171cf305f369d4c96ee7d393
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-11 01:50:26 -0600

    Add tooltips to spotify state

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useLanyardWs } from "use-lanyard";
 import { AnimatePresence, motion } from "framer-motion";
+import { Tooltip } from "react-tippy";
 
 const msToMinSeconds = millis => {
     const minutes = Math.floor(millis / 60000);
@@ -12,8 +13,12 @@ const Spotify = () => {
     const user = useLanyardWs("660292639412846621");
 
     const [current, setCurrent] = React.useState(new Date().getTime());
-    const [elapsed, setElapsed] = React.useState("");
-    const [length, setLength] = React.useState("");
+
+    const [elapsed, setElapsed] = React.useState(NaN);
+    const [elapsedText, setElapsedText] = React.useState("");
+
+    const [length, setLength] = React.useState(NaN);
+    const [lengthText, setLengthText] = React.useState("");
 
     React.useEffect(() => {
         console.log(user);
@@ -21,6 +26,7 @@ const Spotify = () => {
         {
             const length = user.spotify.timestamps.end - user.spotify.timestamps.start;
             setLength(length);
+            setLengthText(msToMinSeconds(length));
         }
     }, [user]);
 
@@ -43,7 +49,8 @@ const Spotify = () => {
             const timeElapsed = current - user.spotify.timestamps.start;
             let width = `${Math.min((timeElapsed / length) * 100, 100)}%`;
             setTimeWidth(width);
-            setElapsed(msToMinSeconds(timeElapsed));
+            setElapsed(timeElapsed);
+            setElapsedText(msToMinSeconds(timeElapsed));
         }
     }, [current]);
 
@@ -65,25 +72,43 @@ const Spotify = () => {
                 </h1>
 
                 <div className="w-full h-[6rem] flex flex-row items-center justify-start">
-                    <img
-                        src={user.spotify.album_art_url}
-                        className="w-[4.5rem] h-[4.5rem] rounded-md mr-4 pointer-events-none"
-                        alt={user.spotify.album}
-                    />
+                    <Tooltip title={user.spotify.album} position={"top"} duration={250} animation={"perspective"}>
+                        <img
+                            src={user.spotify.album_art_url}
+                            className="w-[4.5rem] h-[4.5rem] rounded-md mr-4 pointer-events-none"
+                            alt={user.spotify.album}
+                        />
+                    </Tooltip>
                     <div className="w-56 h-full flex flex-col items-start justify-center">
-                        <a
-                            href={`https://open.spotify.com/track/${user.spotify.track_id}`}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="w-full font-medium text-gray-900 dark:text-[#e1eafd] hover:underline truncate"
-                        >
-                            {user.spotify.song}
-                        </a>
-                        <p className="w-full text-gray-600 dark:text-[#cad2e0] font-normal text-sm truncate">
-                            {user.spotify.artist}
-                        </p>
+                        <Tooltip title={user.spotify.song} position={"top"} duration={250} animation={"perspective"}>
+                            <a
+                                href={`https://open.spotify.com/track/${user.spotify.track_id}`}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="w-full font-medium text-gray-900 dark:text-[#e1eafd] hover:underline truncate"
+                            >
+                                {user.spotify.song}
+                            </a>
+                        </Tooltip>
+                        <Tooltip title={user.spotify.artist} position={"top"} duration={250} animation={"perspective"}>
+                            <p className="w-full text-gray-600 dark:text-[#cad2e0] font-normal text-sm truncate">
+                                {user.spotify.artist}
+                            </p>
+                        </Tooltip>
                         <div className="w-full translate-y-[0.3rem] h-1 bg-[#4f545c]/[.16] min-w-[4px] rounded-sm mb-1">
-                            <div className="h-full bg-white rounded-sm" style={{ width: timeWidth }} />
+                            <Tooltip
+                                position={"top"}
+                                duration={250}
+                                animation={"perspective"}
+                                useContext={true}
+                                html={(
+                                    <div>
+                                        {elapsedText} / {lengthText}
+                                    </div>
+                                )}
+                            >
+                                <div className="h-full bg-white rounded-sm" style={{ width: timeWidth }} />
+                            </Tooltip>
                         </div>
                     </div>
                 </div>
