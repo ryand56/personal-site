6fe99f9b739cc11a314de67a0982bb6416058418
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-14 02:39:26 -0600

    Add contact info

---
+++ src/components/TimeStatus.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+const TimeStatus = () => {
+    const [time, setTime] = React.useState("00:00:00 P.M.");
+    const [awake, setAwake] = React.useState(true);
+
+    const updateTime = () => {
+        let date = new Date();
+        let current = date.toLocaleString("en-US", {
+            timeZone: "America/Edmonton"
+        });
+        setTime(`${current.slice(-10, -6)}${current.slice(-3, -1)}.M.`);
+
+        let hours = date.getHours();
+        if (hours >= 2 && hours < 10) setAwake(false);
+    };
+
+    React.useEffect(() => {
+        updateTime();
+        let interval = setInterval(updateTime, 900);
+
+        return () => {
+            clearInterval(interval);
+            interval = 0;
+        };
+    }, []);
+
+    return (
+        <>
+            <p className="text-black/50 dark:text-white/50 text-sm mb-10">
+                It's currently <span className="font-semibold text-black/60 dark:text-white/60">{time}</span> for me, so I'm
+                probably{" "}
+                <span className="font-semibold text-black/60 dark:text-white/60">{awake ? "awake" : "sleeping"}</span>. I'll
+                get back to you soon.
+                <br />
+                {!awake &&
+                    <span>
+                        Any urgent messages can be forwarded to <a className="font-semibold text-black/60 dark:text-white/60" target="_blank" href="mailto:urgent@emeraldsys.xyz">urgent@emeraldsys.xyz</a>.
+                        I will look at my inbox as soon as I wake up.
+                    </span>
+                }
+            </p>
+        </>
+    );
+};
+
+export default TimeStatus;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 
+import TimeStatus from "../components/TimeStatus";
+
 const Contact = () => {
     return (
         <motion.div
@@ -10,7 +12,14 @@ const Contact = () => {
             transition={{ ease: "easeOut", duration: 0.15 }}
             className="mt-36 w-full"
         >
-            
+            <h1 className="text-black dark:text-white font-bold text-3xl mb-3 mt-8">Let's chat 💬</h1>
+            <p className="text-gray-800 dark:text-gray-200 mb-6">
+                Have an inquiry, or just want to connect and be friends? Feel free to get in touch with me
+                via <a className="font-semibold" target="_blank" href="https://discord.com/users/660292639412846621">Discord</a>,
+                <a className="font-semibold" target="_blank" href="https://twitter.com/elementemerald"> Twitter</a>, or email.
+            </p>
+
+            <TimeStatus />
         </motion.div>
     );
 };
